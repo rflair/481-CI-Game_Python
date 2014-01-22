06990ed8a7221172b0d6aa0497e6e2c04644f9dd
Robby Flair <rflair@clemson.edu>
2014-01-21 19:12:09 -0500

    update with new features

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,8 @@
-print("Hello World!")
+print("Hello world!")
+mood = input("Are you happy or sad today? Enter 'happy' or 'sad' and then press enter:\n")
+if mood == "happy":
+    print("That's great! Goodbye!");
+elif mood == "sad":
+    print("Cheer up and have a great day! Goodbye!");
+else:
+    print("I'm sorry, I do not understand. Either way, have a great day! Goodbye!")
