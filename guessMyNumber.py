6366042cb9d1a27759a9b9381a60769a7169c9fe
Robby Flair <rflair@clemson.edu>
2014-02-13 20:09:12 -0500

    The user is now limited to only 10 guesses in this game

--- guessMyNumber.py
+++ guessMyNumber.py
@@ -1,7 +1,6 @@
-# Guess My Number Game
+# Guess My Number Game Challenge 3
 # The computer picks a random number between 1 and 100.
-# The player tries to guess the number in as few tries
-# as possible.
+# The player tries to guess the number in 10 tries
 # The computer tells the player too high, low, or
 # correct.
 
@@ -9,23 +8,32 @@ import random
 
 print("\tWelcome to 'Guess My Number'!")
 print("\nI'm thinking of a number between 1 and 100.")
-print("Try to guess it in as few attemps as possible!\n")
+print("Try to guess it in only 10 attemps!\n")
 
 # set initial values
 the_number = random.randint(1, 100)
 guess = int(input("Take a guess: "))
 tries = 1
+remaining_tries = 10
 
 # guess loop
-while guess != the_number:
+while tries != 10:
+    tries += 1
+    remaining_tries = remaining_tries - 1
     if guess > the_number:
         print("Lower...")
-    else:
+        print("Remainig number of tries:", remaining_tries)
+    elif guess < the_number:
         print("Higher...")
+        print("Remainig number of tries:", remaining_tries)
+    else:
+        print("Congratulations! You guessed my number and it took you", tries - 1, "tries!")
+        print("The number was:", the_number)
+        break;
     guess = int(input("Take a guess: "))
-    tries += 1
-
-print("Congratulations, you guessed it! The number was", the_number)
-print("And it only took you", tries, "tries!\n")
+    
+if (tries == 10):
+    print("I am sorry, you ran out of tries.")  
+    print("The number i was thinking was:", the_number)
 
 input("\n\nPress the enter key to exit.")
